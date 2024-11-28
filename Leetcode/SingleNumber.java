e799f06572054bb682439b90229930b3affd2656
Ayush Pratap Singh <ayushpratap328@gmail.com>
2024-11-28 23:10:19 +0530

    checking another change by solving single number problem

--- Leetcode/SingleNumber.java
+++ Leetcode/SingleNumber.java
@@ -6,20 +6,19 @@ import java.util.Stack;
 
 public class SingleNumber {
     public static void main(String[] args) {
-        int[] nums = {1};
-        int top=0;
-        Stack <Integer> stack = new Stack<>();
 
-        for (int i = 0; i < nums.length; i++) {
+        int[] nums = {4,1,2,1,2};
+        Arrays.sort(nums);
 
-            stack.push(nums[i]);
+        for (int i = 0; i < nums.length - 1; i+=2) {
+            if(nums[i]!=nums[i+1]){
+                System.out.println(nums[i]);
             }
-
-        for (int i = 0; i < nums.length; i++) {
-
-
-
         }
+
+        System.out.println(nums[nums.length-1]);
+
+
 
     }
 }
