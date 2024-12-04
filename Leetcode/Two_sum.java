1f236c3d874800a65703d973a07fb897ab82b669
Ayush Pratap Singh <ayushpratap328@gmail.com>
2024-12-04 20:59:07 +0530

    changes done in two_sum

--- Leetcode/Two_sum.java
+++ Leetcode/Two_sum.java
@@ -6,25 +6,26 @@ import java.util.HashMap;
 public class Two_sum {
     public static void main(String[] args) {
 
-                int[] nums ={3,3};
+                int[] nums ={3,2,4};
                 int target = 6;
+
+
 
 
                 int[] result =new int[2];
                 HashMap<Integer,Integer> map=new HashMap<>();
-                for(int i=0;i<nums.length;i++){
-                    map.put(nums[i],i);
-                }
 
-                 Arrays.sort(nums);
+
                 int check=0;
                 for(int i=0;i<nums.length;i++){
+
                     check=target-nums[i];
                     if(map.containsKey(check) && map.get(target-nums[i])!=i){
                         result[0]=map.get(target-nums[i]);
                         result[1]=i;
                         break;
                     }
+                    map.put(nums[i],i);
                 }
 
         System.out.println(Arrays.toString(result));
